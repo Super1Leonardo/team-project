b0208613b4c9a42098b9806c115e189b077ce067
pakhrom <124648952+pakhrom@users.noreply.github.com>
2026-03-14 13:23:40 +0300

    feat(web): add Header component

---
+++ web/src/lib/components/Header.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import type { HTMLAttributes } from 'svelte/elements';
+	import { type WithElementRef } from '$lib/utils';
+
+	let {
+		children,
+		class: className,
+		...restProps
+	}: WithElementRef<HTMLAttributes<HTMLHeadingElement>> & {
+		children: Snippet;
+		className: string;
+	} = $props();
+</script>
+
+<h1 class={'mt-8 text-3xl font-semibold ' + className} {...restProps}>
+	{@render children()}
+</h1>
--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+	import Header from '$lib/components/Header.svelte';
+</script>
+
+<Header>Welcome to SvelteKit</Header>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
