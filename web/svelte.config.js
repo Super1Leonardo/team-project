7deac9cc2b1ac4e22cd8c095e38775c46ae7188d
pakhrom <124648952+pakhrom@users.noreply.github.com>
2026-03-14 09:20:54 +0300

    feat(web): enable remote functions

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,12 +1,13 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from "@sveltejs/adapter-node";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: { adapter: adapter() },
-	vitePlugin: {
-		dynamicCompileOptions: ({ filename }) =>
-			filename.includes('node_modules') ? undefined : { runes: true }
-	}
+  kit: { adapter: adapter(), experimental: { remoteFunctions: true } },
+  compilerOptions: { experimental: { async: true } },
+  vitePlugin: {
+    dynamicCompileOptions: ({ filename }) =>
+      filename.includes("node_modules") ? undefined : { runes: true },
+  },
 };
 
 export default config;
