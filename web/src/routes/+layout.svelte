2d62061403cfc807e18bc8e0ac1d89972210cf4f
pakhrom <124648952+pakhrom@users.noreply.github.com>
2026-03-14 22:36:29 +0300

    feat(web): configure svelte-sonner

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -2,11 +2,14 @@
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 	import Header from '$lib/components/Header.svelte';
+	import { Toaster } from 'svelte-sonner';
 
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
+
+<Toaster richColors={true} position="top-right" />
 
 <div class="pt-4 px-8">
 	<Header />
