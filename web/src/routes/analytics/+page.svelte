ddc6418ded1a15429d01983d50387b6e8063b421
pakhrom <124648952+pakhrom@users.noreply.github.com>
2026-03-15 23:00:08 +0300

    feat(web): modify /analytics page's layout

--- web/src/routes/analytics/+page.svelte
+++ web/src/routes/analytics/+page.svelte
@@ -14,33 +14,35 @@
 	let chartData = $derived(data.timeline.filter((day: any) => day.mlConfidence >= minMlScore));
 </script>
 
-<div class="container mx-auto flex max-w-6xl animate-in flex-col gap-6 py-6 duration-500 fade-in">
-	<div class="flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between">
-		<Heading>Аналитика репутации</Heading>
+<div
+	class="container mx-auto flex max-w-6xl animate-in flex-col gap-6 pt-4 pb-6 duration-500 fade-in"
+>
+	<Heading>Аналитика репутации</Heading>
 
-		<div class="flex flex-wrap gap-4">
-			<Select.Root type="single" bind:value={minMlScoreStr}>
-				<Select.Trigger class="w-50">
-					ML Уверенность: &ge; {minMlScoreStr}
-				</Select.Trigger>
-				<Select.Content>
-					<Select.Item value="0.5">&ge; 0.5 (Все данные)</Select.Item>
-					<Select.Item value="0.7">&ge; 0.7 (Базовая норма)</Select.Item>
-					<Select.Item value="0.9">&ge; 0.9 (Высокая точность)</Select.Item>
-				</Select.Content>
-			</Select.Root>
+	<div class="flex items-center flex-wrap gap-4">
+		<div class="text-lg font-semibold">Фильтры:</div>
 
-			<Select.Root type="single" bind:value={timeframe}>
-				<Select.Trigger class="w-40">
-					Период: {timeframe}
-				</Select.Trigger>
-				<Select.Content>
-					<Select.Item value="24h">За 24 часа</Select.Item>
-					<Select.Item value="7d">За 7 дней</Select.Item>
-					<Select.Item value="30d">За 30 дней</Select.Item>
-				</Select.Content>
-			</Select.Root>
-		</div>
+		<Select.Root type="single" bind:value={minMlScoreStr}>
+			<Select.Trigger class="w-50">
+				ML Уверенность: &ge; {minMlScoreStr}
+			</Select.Trigger>
+			<Select.Content>
+				<Select.Item value="0.5">&ge; 0.5 (Все данные)</Select.Item>
+				<Select.Item value="0.7">&ge; 0.7 (Базовая норма)</Select.Item>
+				<Select.Item value="0.9">&ge; 0.9 (Высокая точность)</Select.Item>
+			</Select.Content>
+		</Select.Root>
+
+		<Select.Root type="single" bind:value={timeframe}>
+			<Select.Trigger class="w-40">
+				Период: {timeframe}
+			</Select.Trigger>
+			<Select.Content>
+				<Select.Item value="24h">За 24 часа</Select.Item>
+				<Select.Item value="7d">За 7 дней</Select.Item>
+				<Select.Item value="30d">За 30 дней</Select.Item>
+			</Select.Content>
+		</Select.Root>
 	</div>
 
 	<div class="grid grid-cols-1 gap-4 md:grid-cols-3">
